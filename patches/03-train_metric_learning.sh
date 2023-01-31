e8a264f09c25c5d5aaf59242daf25d6a960a1dfd
Vilém Zouhar <vilem.zouhar@gmail.com>
2023-01-31 11:17:24 +0100

    uncomment patches

--- patches/03-train_metric_learning.sh
+++ patches/03-train_metric_learning.sh
@@ -2,25 +2,23 @@
 
 mkdir -p computed/models
 
-# hack to overshadow system LDPATH with custom bashrc
-# source /cluster/home/vzouhar/.bashrc; 
 for FEATURES in "panphon" "tokenort" "tokenipa"; do
-    # for LANG in 'en' 'am' 'bn' 'uz' 'pl' 'es' 'sw'; do
-    #     SIGNATURE="train_rnn_${FEATURES}_${LANG}"
-    #     sbatch --time=01-00 --ntasks=12 --mem-per-cpu=4G --gpus=1 \
-    #         --job-name="${SIGNATURE}" \
-    #         --output="logs/${SIGNATURE}.log" \
-    #         --wrap="CUDA_VISIBLE_DEVICES=0 python3 \
-    #             ./models/metric_learning/train.py \
-    #                 --data \"data/multi.tsv\" \
-    #                 --lang ${LANG} \
-    #                 --save-model-path \"computed/models/rnn_metric_learning_${FEATURES}_${LANG}.pt\" \
-    #                 --number-thousands 200 \
-    #                 --target-metric \"l2\" \
-    #                 --features ${FEATURES} \
-    #                 --epochs 30 \
-    #             ;"
-    # done;
+    for LANG in 'en' 'am' 'bn' 'uz' 'pl' 'es' 'sw'; do
+        SIGNATURE="train_rnn_${FEATURES}_${LANG}"
+        sbatch --time=01-00 --ntasks=12 --mem-per-cpu=4G --gpus=1 \
+            --job-name="${SIGNATURE}" \
+            --output="logs/${SIGNATURE}.log" \
+            --wrap="CUDA_VISIBLE_DEVICES=0 python3 \
+                ./models/metric_learning/train.py \
+                    --data \"data/multi.tsv\" \
+                    --lang ${LANG} \
+                    --save-model-path \"computed/models/rnn_metric_learning_${FEATURES}_${LANG}.pt\" \
+                    --number-thousands 200 \
+                    --target-metric \"l2\" \
+                    --features ${FEATURES} \
+                    --epochs 20 \
+                ;"
+    done;
     
     for LANG in 'multi'; do
         SIGNATURE="train_rnn_${FEATURES}_${LANG}"
@@ -35,11 +33,9 @@ for FEATURES in "panphon" "tokenort" "tokenipa"; do
                     --number-thousands 400 \
                     --target-metric \"l2\" \
                     --features ${FEATURES} \
-                    --epochs 30 \
+                    --epochs 20 \
                 ;"
     done;
-
-
 done;
 
 # FEATURES=tokenort
--- patches/07-evaluate_all.sh
+++ patches/07-evaluate_all.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/bash
 
 for FEATURES in "panphon" "tokenort" "tokenipa"; do
-    # SIGNATURE="eval_all_rnn_${FEATURES}"
-    # sbatch --time=00-01 --ntasks=30 --mem-per-cpu=1G  \
-    #     --job-name="${SIGNATURE}" \
-    #     --output="logs/${SIGNATURE}.log" \
-    #     --wrap="\
-    #         ./suite_evaluation/eval_all.py \
-    #             --embd \"computed/embd_rnn_metric_learning/${FEATURES}.pkl\" \
-    #         ;"
+    SIGNATURE="eval_all_rnn_${FEATURES}"
+    sbatch --time=00-01 --ntasks=30 --mem-per-cpu=1G  \
+        --job-name="${SIGNATURE}" \
+        --output="logs/${SIGNATURE}.log" \
+        --wrap="\
+            ./suite_evaluation/eval_all.py \
+                --embd \"computed/embd_rnn_metric_learning/${FEATURES}.pkl\" \
+            ;"
 
-    # SIGNATURE="eval_all_rnn_${FEATURES}_multi"
-    # sbatch --time=00-01 --ntasks=30 --mem-per-cpu=1G  \
-    #     --job-name="${SIGNATURE}" \
-    #     --output="logs/${SIGNATURE}.log" \
-    #     --wrap="\
-    #         ./suite_evaluation/eval_all.py \
-    #             --embd \"computed/embd_rnn_metric_learning/${FEATURES}_fmulti.pkl\" \
-    #         ;"
+    SIGNATURE="eval_all_rnn_${FEATURES}_multi"
+    sbatch --time=00-01 --ntasks=30 --mem-per-cpu=1G  \
+        --job-name="${SIGNATURE}" \
+        --output="logs/${SIGNATURE}.log" \
+        --wrap="\
+            ./suite_evaluation/eval_all.py \
+                --embd \"computed/embd_rnn_metric_learning/${FEATURES}_fmulti.pkl\" \
+            ;"
 
     SIGNATURE="eval_mismatch_rnn_${FEATURES}"
     sbatch --time=01-00 --ntasks=100 --mem-per-cpu=500M  \
