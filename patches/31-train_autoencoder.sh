8684553dbe1b3d16af02a08349359d0b6d7f4c20
Vilém Zouhar <vilem.zouhar@gmail.com>
2023-02-01 17:42:41 +0100

    restructure data, add custom autoencoder

---
+++ patches/31-train_autoencoder.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+mkdir -p computed/models
+
+for FEATURES in "panphon" "tokenort" "tokenipa"; do
+    for LANG in 'en' 'am' 'bn' 'uz' 'pl' 'es' 'sw'; do
+        SIGNATURE="train_rnn_autoencoder_${FEATURES}_${LANG}"
+        sbatch --time=01-00 --ntasks=12 --mem-per-cpu=4G --gpus=1 \
+            --job-name="${SIGNATURE}" \
+            --output="logs/${SIGNATURE}.log" \
+            --wrap="CUDA_VISIBLE_DEVICES=0 python3 \
+                ./models/autoencoder/train.py \
+                    --data \"data/multi.tsv\" \
+                    --lang ${LANG} \
+                    --save-model-path \"computed/models/rnn_autoencoder_${FEATURES}_${LANG}.pt\" \
+                    --number-thousands 200 \
+                    --features ${FEATURES} \
+                    --epochs 20 \
+                ;"
+    done;
+    
+done;
