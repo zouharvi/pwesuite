f4147ef460f923d4afae7f5802b15e9da5d07eba
Vilém Zouhar <vilem.zouhar@gmail.com>
2023-02-02 21:15:50 +0100

    fix human similarity data

--- patches/11-apply_baselines.sh
+++ patches/11-apply_baselines.sh
@@ -8,22 +8,9 @@ for MODEL in "bert"; do
         --wrap="\
             ./models/baselines/${MODEL}_embd.py \
         ;"
-
-    # SIGNATURE="eval_all_rnn_${FEATURES}_multi"
-    # sbatch --time=00-01 --ntasks=30 --mem-per-cpu=1G  \
-    #     --job-name="${SIGNATURE}" \
-    #     --output="logs/${SIGNATURE}.log" \
-    #     --wrap="\
-    #         ./suite_evaluation/eval_all.py \
-    #             --embd \"computed/embd_rnn_metric_learning/${FEATURES}_fmulti.pkl\" \
-    #         ;"
-
-    # SIGNATURE="eval_mismatch_rnn_${FEATURES}"
-    # sbatch --time=01-00 --ntasks=100 --mem-per-cpu=500M  \
-    #     --job-name="${SIGNATURE}" \
-    #     --output="logs/${SIGNATURE}.log" \
-    #     --wrap="\
-    #         ./suite_evaluation/eval_mismatch.py \
-    #             --embd \"computed/embd_rnn_metric_learning/${FEATURES}_fLANG.pkl\" \
-    #         ;"
 done;
+
+
+# for MODEL in "bpemb" "fasttext" "instructor"; do
+#     python3 ./models/baselines/${MODEL}_embd.py
+# done;
