ef3a84af69239d512e6093a48db4f7f4c297220b
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-04-28 22:37:55 +0200

    add convertor script

---
+++ scripts/42-convert-to-jsonl.py
@@ -0,0 +1,12 @@
+import json
+
+data = [line.rstrip().split("\t") for line in open("data/multi.tsv", "r")]
+
+with open("data/train.jsonl", "w") as f:
+    f.write("\n".join([json.dumps({
+        "token_ort": x[0],
+        "token_ipa": x[1],
+        "token_arp": x[4],
+        "lang": x[2],
+        "purpose": x[3],
+    }, ensure_ascii=False) for x in data]))
