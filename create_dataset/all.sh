24e9fbca7b0a3aa8284e5eb9ae4ef833e76913d0
James Kay <jka87@uclive.ac.nz>
2025-02-18 11:44:51 +1300

    updated script to use 2.0.0 release which is available from the cmuflite website

--- create_dataset/all.sh
+++ create_dataset/all.sh
@@ -8,17 +8,17 @@ for LANG in 'am' 'bn' 'uz' 'pl' 'es' 'sw' 'fr' 'de'; do
 done
 
 wget -nc "https://github.com/Alexir/CMUdict/raw/master/cmudict-0.7b" -O "data/raw/cmudict.tmp"
-# th;e file is not encoded correctly so make sure it's that and invalid codepoints are skipped (-c)
+# the file is not encoded correctly so make sure it's that and invalid codepoints are skipped (-c)
 iconv -c -t UTF-8 "data/raw/cmudict.tmp" > "data/raw/cmudict-0.7b.txt"
 wget -nc "https://github.com/menelik3/cmudict-ipa/raw/master/cmudict-0.7b-ipa.txt" -O "data/raw/cmudict-0.7b-ipa.txt"
 
 # for English epitran
 if ! command -v /usr/local/bin/lex_lookup &> /dev/null
 then
-  wget http://tts.speech.cs.cmu.edu/awb/flite-2.0.5-current.tar.bz2 -O data/raw/flite-2.0.5-current.tar.bz2
+  wget http://cmuflite.org/packed/flite-2.0/flite-2.0.0-release.tar.bz2 -O data/raw/flite-2.0.0-release.tar.bz2
   cd data/raw
-  tar xjf flite-2.0.5-current.tar.bz2
-  cd flite-2.0.5-current
+  tar xjf flite-2.0.0-release.tar.bz2
+  cd flite-2.0.0-release
   if [ "$(uname)" == "Darwin" ]; then
   sed -i.bak "s/cp \-pd/cp \-pR/g" main/Makefile #this command only needs to run if it is on a MacOS machine.
   fi
@@ -30,11 +30,10 @@ then
   cd ../../../..
 fi
 
-# wget -nc https://raw.githubusercontent.com/kbatsuren/CogNet/master/CogNet-v0.tsv -O data/raw/CogNet-v0.tsv
+wget -nc https://raw.githubusercontent.com/kbatsuren/CogNet/master/CogNet-v0.tsv -O data/raw/CogNet-v0.tsv
 
 python3 ./create_dataset/preprocess.py
 python3 ./create_dataset/add_analogies.py
 python3 ./create_dataset/add_human_similarity.py
 python3 ./create_dataset/add_cognates.py
-
 python3 ./create_dataset/finalize_hf_jsonl.py
